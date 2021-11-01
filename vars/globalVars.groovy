bac82484cd3860936576bb6777b11ff61c71c558
Vikash082 <vikash.kumar@oneconvergence.com>
2021-11-01 09:11:02 +0530

    tranformfield

--- vars/globalVars.groovy
+++ vars/globalVars.groovy
@@ -1,16 +1,3 @@
-environment {
-    def accpImage
-
-    def remote = [:]
-    remote.allowAnyHosts = true
-
-    def configHost = [host: 'noiro-containers-ctrl']
-    jumpHost = [:]
-    // for openshift cluster, skip acc provisioning.
-    skipAccProvisionClusters = ["fab3", "k8s-bm-1", "os-bm-1", "k8sbm2-bm"]
-    hypOpenshift = ["os1", "os2", "os3", "os4", "os5"]
-    hypK8s = []
-    hyperflex = [openshift: false, k8s: false]
-
-    base = "/home/noiro/acc-pytests-jobs-config"
-}
+@groovy.transform.Field
+def configHost = [host: 'noiro-containers-ctrl']
+skipAccProvisionClusters = ["fab3", "k8s-bm-1", "os-bm-1", "k8sbm2-bm"]
--- vars/hello.groovy
+++ vars/hello.groovy
@@ -3,7 +3,6 @@
 def call(String name = 'human') {
   echo "Hello, ${name}."
   world()
-  println remote
-  println hypOpenshift
-  println base
+  println configHost
+  println skipAccProvisionClusters
 }
